22edfa07a076ea33757ef806556f1a416ff0f01a
Victor Jian Choe <vchoe89@gmail.com>
2019-07-22 02:07:05 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 
-def meal_choice(veg1, veg2, protein="meat")
+def meal_choice(veg1, veg2, meat="protein")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
